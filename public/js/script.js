3e4eb87b1fc6be8b70e527a92829713686d04d32
Dayvson Spacca <spacca.dayvson@gmail.com>
2023-09-05 17:00:24 -0300

    :tada: feat(): Commit inicial.

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+
+
+async function getRandomFact() {
+    const lang = document.getElementById('lang').value;
+    const randomizeButton = document.getElementById('randomize')
+
+    randomizeButton.disabled = true;
+
+    const response = await fetch("https://meowfacts.herokuapp.com/?lang=" + lang);
+    const factResponse = await response.json();
+    const fact = factResponse.data[0];
+
+    const factsContainer = document.getElementById('facts');
+
+    let divFact = document.createElement('div');
+
+    divFact.className = 'fact';
+    divFact.innerText = fact
+
+    factsContainer.appendChild(divFact)
+
+    setTimeout( () => {
+        randomizeButton.disabled = false;
+    }, 1000)
+}
